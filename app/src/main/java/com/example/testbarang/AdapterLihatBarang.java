145da5b793a7daa1554eeec3f7d8e6d313d9feb0
Nando Alhasanain <Nandoalhasanain@gmail.com>
2021-06-24 10:10:53 +0700

    commit Update dan Delete data

--- app/src/main/java/com/example/testbarang/AdapterLihatBarang.java
+++ app/src/main/java/com/example/testbarang/AdapterLihatBarang.java
@@ -1,17 +1,30 @@
 package com.example.testbarang;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.content.Context;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class AdapterLihatBarang extends RecyclerView.Adapter<AdapterLihatBarang.ViewHolder> {
     private ArrayList<Barang> daftarBarang;
     private Context context;
+    private DatabaseReference databaseReference;
+
     public AdapterLihatBarang(ArrayList<Barang> barangs, Context ctx){
         /**
          * Inisiasi data dan variabel yang akan digunakan
@@ -29,6 +42,8 @@ public class AdapterLihatBarang extends RecyclerView.Adapter<AdapterLihatBarang.
         ViewHolder(View v) {
             super(v);
             tvTitle = (TextView) v.findViewById(R.id.tv_namabarang);
+
+            databaseReference = FirebaseDatabase.getInstance().getReference();
         }
     }
     @Override
@@ -36,36 +51,70 @@ public class AdapterLihatBarang extends RecyclerView.Adapter<AdapterLihatBarang.
         /**
          * Inisiasi ViewHolder
          */
-        View v =
-                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent,
-                        false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent, false);
         // mengeset ukuran view, margin, padding, dan parameter layout lainnya
         ViewHolder vh = new ViewHolder(v);
         return vh;
-    } @Override
+    }
+
+    @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        /**
-         * Menampilkan data pada view
-         */
         final String name = daftarBarang.get(position).getNama();
-        holder.tvTitle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /**
-                 * untuk latihan Selanjutnya , jika ingin membaca detail data
-                 */
-            }
-        });
+        String kode;
+        kode = daftarBarang.get(position).getKode();
+        holder.tvTitle.setText(name);
         holder.tvTitle.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public boolean onLongClick(View view) {
-                /**
-                 * untuk latihan Selanjutnya ,fungsi Delete dan Update data
-                 */
+            public boolean onLongClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(v.getContext(),v);
+                popupMenu.inflate(R.menu.menuteman);
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.mnEdit:
+                                Bundle bundle = new Bundle();
+                                bundle.putString("kunci2", name);
+                                bundle.putString("kunci3", kode );
+
+                                Intent intent = new Intent(v.getContext(),TemanEdit.class);
+                                intent.putExtras(bundle);
+                                v.getContext().startActivity(intent);
+                                break;
+
+                            case R.id.mnHapus:
+                                AlertDialog.Builder alerDlg = new AlertDialog.Builder(v.getContext());
+                                alerDlg.setTitle("Yakin data akan dihapus?");
+                                alerDlg.setMessage("tekan 'Ya' untuk menghapus")
+                                        .setCancelable(false)
+                                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if(databaseReference !=null){
+                                                    databaseReference.child("Barang").child(kode).removeValue();
+                                                }
+                                                Toast.makeText(v.getContext(),"Data " +name+" berhasil dihapus",Toast.LENGTH_LONG).show();
+                                                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                                                v.getContext().startActivity(intent);
+                                            }
+                                        }).setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                                AlertDialog aDlg = alerDlg.create();
+                                aDlg.show();
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
                 return true;
             }
         });
-        holder.tvTitle.setText(name);
     }
     @Override
     public int getItemCount() {
